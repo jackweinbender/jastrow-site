22c41d406a91bbdba404181d81e9cd58bd66168d
Jack Weinbender <jack.weinbender@gmail.com>
2018-02-05 14:57:56 -0600

    initial migration for dictionary

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+    number = models.IntegerField()
+    headword = models.TextField()
+
+class Section(models.Model):
+    title = models.TextField()
+
+class Letter(models.Model):
+    language = models.TextField()
+    char = models.TextField()
+    translit = models.TextField()
+    ascii_translit = models.TextField()
+    name = models.TextField()
+    sort = models.IntegerField()

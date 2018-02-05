d38ef15e2b08f58149eff4c6b4eb9c0639397e18
Jack Weinbender <jack.weinbender@gmail.com>
2018-02-05 16:10:17 -0600

    Making progress on basic admin

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from dictionary.models import Page,Letter,Section
 # Register your models here.
+  
+class PageAdmin(admin.ModelAdmin):
+  search_fields = ['number']
+  ordering = ('number',)
+admin.site.register(Page, PageAdmin)
+
+class LetterAdmin(admin.ModelAdmin):  
+  ordering = ('sort_order',)
+admin.site.register(Letter, LetterAdmin)
+
+class SectionAdmin(admin.ModelAdmin):  
+  ordering = ('sort_order',)
+admin.site.register(Section, SectionAdmin)
--- dictionary/models.py
+++ dictionary/models.py
@@ -3,16 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Page(models.Model):
-    number = models.IntegerField()
-    headword = models.TextField()
+    number = models.IntegerField(unique=True)
+    headword = models.CharField(max_length=256, blank=True)
+    def __str__(self):
+        return f"{self.number} - {self.headword}"
 
 class Section(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=256)
+    sort_order = models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.sort_order} - {self.title}"
 
 class Letter(models.Model):
-    language = models.TextField()
-    char = models.TextField()
-    translit = models.TextField()
-    ascii_translit = models.TextField()
-    name = models.TextField()
-    sort = models.IntegerField()
+    language = models.CharField(max_length=256)
+    char = models.CharField(max_length=256)
+    translit = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    sort_order = models.IntegerField()
+    def __str__(self):
+        return f"{self.language}: {self.char} - {self.name}"

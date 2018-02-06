f0f0013d01eb5b8e47e85cf1b9435433a123b035
Jack Weinbender <jack.weinbender@gmail.com>
2018-02-06 11:19:29 -0600

    Added static assets

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,7 +1,7 @@
 from bakery.views import BuildableListView,BuildableDetailView
 from dictionary.models import Letter,Page
 
-IMAGE_URL_PATH = "https://storage.googleapis.com/dictionary-nearline/images/"
+IMAGE_URL_PATH = "/static/dictionary/pages/"
 
 class IndexView(BuildableListView):
     model = Letter
@@ -22,7 +22,7 @@ class PageView(BuildableDetailView):
         context = super().get_context_data(**kwargs)
         
         page = kwargs['object']
-        file_name = "page_%03d/%03d_original.png" % (page.number,page.number)
+        file_name = "%03d_original.png" % (page.number,)
         
         context['letters'] = Letter.objects.all()
         context['pages'] = Page.objects.filter(letter_id=page.letter_id)
